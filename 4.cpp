e381ac17901b8144c8d783c161ca733949e9e2cf
Abhik Sahu <abhiksahu2003@gmail.com>
2023-12-17 19:23:29 +0530

    lists in STL

--- 4.cpp
+++ 4.cpp
@@ -2,13 +2,3 @@
 using namespace std;
 /*Lists using stl*/
 /*list elements cannot be accessed directly as it was in the case of deque,arrays and vectors*/
-
-int main(){
-    list<int> listitem;
-    listitem.push_back(1);
-    listitem.push_front(45);
-
-    for(int i:listitem){
-        cout<<i<<endl;
-    }
-}
